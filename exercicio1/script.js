4f65653b791b71e56f5cf40bef31d0b0eff99050
Daniel <daniel.sprcp@gmail.com>
2022-07-21 15:09:01 -0300

    finalizado exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+// EXERCICIO 1:
+
+// ITEM A:
+
+const numero = prompt("Insira um número par:")
+
+// ITEM B:
+
+console.log("O resto da divisão deste número por 2 é:", Number(numero % 2))
+
+// ITEM C:
+
+// Sempre que um número par é dividido por 2 o resto da divisão é igual a 0.
+
+// ITEM D:
+
+// Quando o usuário insere um número ímpar, o resto da divisão deste número por 2 é igaul a 1.
+
+
+
