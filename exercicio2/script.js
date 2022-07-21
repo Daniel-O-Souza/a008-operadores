33e8a130ce93ad7d7d8a94416e349129fc4d021e
Daniel <daniel.sprcp@gmail.com>
2022-07-21 15:21:56 -0300

    finalizado exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+// EXERCICIO 2:
+
+// IMPRIMINDO VARIAVEIS:
+
+const numero1 = prompt("Digite o primeiro número:")
+
+const numero2 = prompt("Digite o segundo número:")
+
+// IMPRIMINDO MENSAGENS NA TELA:
+
+console.log("O primeiro número é maior que o segundo?", numero1 > numero2)
+
+console.log("O primeiro número é igual ao segundo?", numero1 === numero2)
+
+console.log("O primeiro numero é divisível pelo segundo?", (numero1 % numero2) === 0)
+
+console.log("O segundo numero é divisível pelo primeiro?", (numero2 % numero1) === 0)
+
